1e93ad945da578a89177c89a892cd510902890fc
Layne <Layne@qq.com>
2022-09-13 17:59:27 +0800

    git2

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,6 +1,8 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("aaa");
         System.out.println("hello,get2");
+        System.out.println("git2");
     }
 }
